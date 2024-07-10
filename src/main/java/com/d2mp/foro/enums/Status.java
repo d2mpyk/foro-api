d31d863b54fd1fc6c17532998ff1645680a07b16
d2mpyk <d2mpyk@gmail.com>
2024-07-10 02:06:25 -0500

    Enum Status check

---
+++ src/main/java/com/d2mp/foro/enums/Status.java
@@ -0,0 +1,20 @@
+package com.d2mp.foro.enums;
+
+public enum Status {
+    ABIERTO("Abierto"),
+    CERRADO("Cerrado"),
+    SOLUCIONADO("Solucionado");
+
+    private String status;
+
+    Status (String status){ this.status = status;}
+
+    public static Status fromString(String text){
+        for(Status status : Status.values()){
+            if(status.status.equalsIgnoreCase(text)){
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Este status no fue encontrado: " + text);
+    }
+}
